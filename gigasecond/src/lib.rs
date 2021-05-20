570fd8ddbf27a616996bc5d3580af810543a9024
ederst <stefan.prietl@bearingpoint.com>
2021-05-20 14:20:58 +0200

    Calculate the gigasecond const

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,6 +1,7 @@
 use chrono::{DateTime, Utc, Duration};
 
-const GIGASECOND: i64 = 1_000_000_000;
+//const GIGASECOND: i64 = 1_000_000_000;
+const GIGASECOND: i64 = i64::pow(10, 9);
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
