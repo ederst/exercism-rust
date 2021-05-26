00e4fa05c21d2b2952c66dcf7ad0a2ab9d7dc581
ederst <stefan.prietl@bearingpoint.com>
2021-05-26 10:19:56 +0200

    Shorten 'armstrong-numbers' solution

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,9 +1,7 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    let num_string = num.to_string();
-
-    num_string
+    num.to_string()
         .chars()
-        .map(|x| x.to_digit(10).unwrap().pow(num_string.len() as u32))
+        .map(|x| x.to_digit(10).unwrap().pow(num.to_string().len() as u32))
         .sum::<u32>()
         == num
 }
