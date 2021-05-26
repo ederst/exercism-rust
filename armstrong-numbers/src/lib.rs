ca4764c6eedd41bbb5fe5d6b06d1aa0743adca88
ederst <stefan.prietl@bearingpoint.com>
2021-05-26 10:18:17 +0200

    Add first solution of 'armstrong-numbers'

---
+++ armstrong-numbers/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn is_armstrong_number(num: u32) -> bool {
+    let num_string = num.to_string();
+
+    num_string
+        .chars()
+        .map(|x| x.to_digit(10).unwrap().pow(num_string.len() as u32))
+        .sum::<u32>()
+        == num
+}
